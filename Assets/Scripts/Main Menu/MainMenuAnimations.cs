09d811d871c2be207326c65b5d1f6375275ac183
Adriel Perez <adriel_campana@hotmail.com>
2023-11-20 22:45:16 -0700

    Added Main Menu
    Added functional button that starts game and changes scene to 'level' scene.
    Stored level assets into separate folders

---
+++ Assets/Scripts/Main Menu/MainMenuAnimations.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class MainMenuAnimations : MonoBehaviour, IPointerEnterHandler, 
+                                                             IPointerExitHandler
+    {
+    [ SerializeField ] private TextMeshProUGUI buttonText;
+    [ SerializeField ] private int hoverSize;
+    [ SerializeField ] private int normalSize;
+    private bool isHovered = false;
+    public int transitionSpeed;
+
+    private void Update()
+        {
+        if (isHovered)
+            {
+            // Smoothly increase the font size
+            buttonText.fontSize = Mathf.Lerp( buttonText.fontSize, hoverSize, 
+                                             Time.deltaTime * transitionSpeed );
+            }
+
+        else
+            {
+            // Smoothly decrease the font size
+            buttonText.fontSize = Mathf.Lerp( buttonText.fontSize, normalSize, 
+                                             Time.deltaTime * transitionSpeed );
+            }
+
+        }
+
+    public void OnPointerEnter(PointerEventData eventData)
+        {
+        isHovered = true;
+        }
+
+    public void OnPointerExit(PointerEventData eventData)
+        {
+        isHovered = false;
+        }
+        
+}
